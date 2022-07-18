1ceb71865e483dd9fb290166f0fa39b30ed34e65
Sophie <sophie.nye@gmail.com>
2022-07-18 13:02:08 -0400

    Initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        main: "./src/index.js"
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            },
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 8080,
+        publicPath: "http://localhost:8080/dist/",
+        hotOnly: true,
+        proxy: [{
+            context: ['/todo'],
+            target: 'http://localhost:3000',
+        }],
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    node: {
+        fs: "empty"
+    }
+};
